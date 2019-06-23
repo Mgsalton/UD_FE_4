1744c02e6b689a9afb20d5f63241010cfc875fc7
Mgsalton <mgsalton@gmail.com>
2019-06-23 15:35:39 +0100

    Successfully implemented:
    - code commented, ready to release!

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,3 +1,9 @@
+/******************************************************************************
+
+This section is the default code provided by Udacity. No edits made!
+
+******************************************************************************/
+
 /* feedreader.js
  *
  * This is the spec file that Jasmine will read and contains
@@ -21,40 +27,58 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
-        });
 
-        /* DONE: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-         /* DONE: Write a test that loops through each feed
-          * in the allFeeds object and ensures it has a name defined
-          * and that the name is not empty.
-          */
+         it('are defined', function() {
+             expect(allFeeds).toBeDefined();
+             expect(allFeeds.length).not.toBe(0);
+         });
+
+/******************************************************************************
+
+!!!.Start of code.!!!
+
+1.  We'll first write a test that loops through each feed
+    in the allFeeds object and makes sure it has a URL defined
+    and that the URL is not empty.
+
+2.  We'll then proceed to write a test that loops through each feed
+    in the allFeeds object and makes sure that it has a name defined
+    and that the name is not empty.
+
+******************************************************************************/
+
+        //  Our second spec under the 'RSS Feeds' suite is defined here.
+        //    3. The ForEach method is used to loop over the allFeeds entities
+        //       defined in app.js.
+        //
+        //    4. The first expectations declared below rely upon the counter
+        //       variable to reference the correct array entity during loops.
+        //       It is incremented by 1 after each loop at the very bottom.
 
         it('have defined URLs', function() {
-            let url = '';
+            //let url = '';
             let counter = 0;
 
             allFeeds.forEach(function() {
-                url = allFeeds[counter].url;
-                console.log('The URL is ' + url);
+                //url = allFeeds[counter].url;
+                //console.log('The URL is ' + url);
                 expect(allFeeds[counter].url).toBeDefined();
                 expect(allFeeds[counter].url.length).not.toBe(0);
                 counter += 1;
             });
         });
 
+        //  The third spec under the 'RSS Feeds' suite is defined here.
+        //    5. It's virtually identical to the spec above, but calls
+        //       the 'name' property of the allFeeds object instead of the URL
+
         it('have defined names', function() {
-            let name = '';
+            //let name = '';
             let counter = 0;
 
             allFeeds.forEach(function() {
-                name = allFeeds[counter].name;
-                console.log('The name is ' + name);
+                //name = allFeeds[counter].name;
+                //console.log('The name is ' + name);
                 expect(allFeeds[counter].name).toBeDefined();
                 expect(allFeeds[counter].name.length).not.toBe(0);
                 counter += 1;
@@ -62,19 +86,23 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "The menu" */
+/******************************************************************************
 
-        /* DONE: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+    6.  We'll now write a test that ensures the menu element is
+        hidden by default.
 
-         /* DONE: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+    7.  We'll then write a test that ensures that the menu changes
+        visibility when the menu icon is clicked. This test
+        will have two expectations: does the menu display when
+        clicked and does it hide when clicked again.
+
+******************************************************************************/
+
+    // 8. 'describe' instantiates a new suite which we'll call 'The menu'.
+    //     We're going to use querySelectors to identify the icon that
+    //     shows and hides the menu (which was found by checking app.js).
+    // 9.  The .click() method will simulate the mouse clicking on the menu icon
+    //     ass opposed to using eventListeners (since this is automated).
 
     describe('The menu', function() {
 
@@ -98,14 +126,23 @@ $(function() {
         });
     });
 
-    /* DONE: Write a new test suite named "Initial Entries" */
+/******************************************************************************
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        10.  We'll now write a test suite called 'Initial Entries'
+
+        11.  And proceed to write a test that ensures that when the loadFeed
+             function is called and completes its work, there is at least
+             a single .entry element within the .feed container.
+             Since loadFeed() is asynchronous, this test will require
+             the use of Jasmine's beforeEach and asynchronous done() function.
+
+******************************************************************************/
+
+
+    // Since this code is asynchronous, we'll leverage beforeEach and the
+    // 'done' callback to ensure the code executes fully before moving on.
+    // The setTimout function is also declared to simulate asynchronous
+    // behaviour
 
     describe('Initial Entries', function() {
 
@@ -122,50 +159,75 @@ $(function() {
         });
     });
 
-    /* DONE: Write a new test suite named "New Feed Selection" */
+/******************************************************************************
 
-    /* DONE: Write a test that ensures when a new feed is loaded
-    * by the loadFeed function that the content actually changes.
-    * Remember, loadFeed() is asynchronous.
-    */
+        12.  Finally, we'll write a new test suite named "New Feed Selection"
+             and write a test that ensures that when a new feed is loaded
+             by the loadFeed function that the content actually changes.
+
+******************************************************************************/
+
+    // this suite is a lot more verbose than the ones preceeding it, so
+    // each section will be commented out in turn:
+
+
+    // This section defines the suite and declares block-scope variables.
+    // const feed: holds the querySelector for the .feed class
+    // feedOne/Two: are placeholder arrays that will come in handy later
 
     describe('New Feed Selection', function() {
-
         const feed = document.querySelector('.feed');
         let feedOne = [];
         let feedTwo = [];
 
+        // feedOneArray is called alongside loadFeed in the beforeEach function.
+        // Its purpose is to store the contents of the first feed that is loaded
+        // into an array so that it can be compared to the second feed later.
+
         function feedOneArray() {
             Array.from(feed.children).forEach(function(id) {
                 feedOne.push(id.innerText);
-                console.log('First array: ' + feedOne);
+                //console.log('First array: ' + feedOne);
             });
         };
+
+        // As above, except this function is called slightly later to prevent
+        // an async timeout in the beforeEach function.
 
         function feedTwoArray() {
             Array.from(feed.children).forEach(function(id) {
                 feedTwo.push(id.innerText);
-                console.log('Second array: ' + feedTwo);
+                //console.log('Second array: ' + feedTwo);
             });
         };
+
+        // This section simulates asynchronous behaviour. It has a timeout
+        // function and leverages the 'done' callback to ensure that then
+        // interpreter doesn't go off on a tangent.
 
         beforeEach(function(done) {
             setTimeout(function() {
                 loadFeed(0, feedOneArray);
-                //console.log(feed.children[0]);
                 loadFeed(1, done);
             }, 1000);
         });
+
+        // feedOneArray is called from BeforeEach, while feedTwoArray is called
+        // from within the spec. Again, this was done to avoid an async timout.
 
         it('updates the content of the feed',function() {
             feedTwoArray();
             let count = 0;
             let boundary = feedTwo.length;
-            console.log('! Count length is : ' + boundary);
+            //console.log('! Count length is : ' + boundary);
+
+            // A for loop is used to iterate between the arrays of feedOne and
+            // feedTwo respectively, make a comparison, and (hopefully) return
+            // false to signify that both values from the arrays don't match
 
             for (count = 0; count < boundary; count++ ) {
-                console.log('! Contents of first array: ' + feedOne[count]);
-                console.log('! Contents of second array: ' + feedTwo[count]);
+                //console.log('! Contents of first array: ' + feedOne[count]);
+                //console.log('! Contents of second array: ' + feedTwo[count]);
                 expect(feedOne[count] === feedTwo[count]).toBe(false);
             };
         });
